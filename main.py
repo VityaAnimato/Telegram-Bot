b942ce087e6ba1d94fd3fa987ab4c748886f5071
vitya123 <95485489+VityaAnimato@users.noreply.github.com>
2022-05-26 12:25:50 +0000

    connect to cryptocompare API

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
 import telebot
-import config 
+import config
+import json
+import requests
+
+bot = telebot.TeleBot(config.TOKEN)
 
 
-bot = telebot.TeleBot(config.token)
+keys = {
+    'dollar': 'USD',
+    'euro': 'EUR',
+    'ruble': 'RUB',
+}
+
 
 
 @bot.message_handler(commands=["start", "help"])
 def start(m, res=False):
-    bot.send_message(m.chat.id, 'Воспользуйтесь следующими командами:')
-    bot.send_message(m.chat.id, '/values - валюты, доступные для конвертации')
-    bot.send_message(m.chat.id, '/calc - конвертировать валюту')
+    bot.send_message(m.chat.id, "Для конвертации валюты - введите запрос в следующем формате: \
+                     \n\n <валюта#1> <валюта#2> <кол-во валюты#1> \
+                     \n\n например: 'euro dollar 17' \
+                     \n\n /values - валюты, доступные для конвертации")
+                    
+
+
+
 
 
 @bot.message_handler(commands=["values"])
 def values(m, res=False):
-    bot.send_message(m.chat.id, 'Dollar, Euro, Ruble')
-    bot.send_message(m.chat.id, '/calc - конвертировать валюту')
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key,))
+    bot.reply_to(m, text)
+
+
+@bot.message_handler(content_types=["text", ])
+def convert(m, res=False):
+    quote, base, amount = m.text.split(' ')
+    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={keys[quote]}&tsyms={keys[base]}')
+    total_base = json.loads(r.content)[keys[base]]
+    text = f'Цена {amount} {quote} в {base} - {total_base}'
+    bot.send_message(m.chat.id, text)
 
 
 @bot.message_handler(commands=["calc"])
@@ -27,4 +52,5 @@ def calc(m, res=False):
 
     
 # Запускаем бота
+    
 bot.polling(none_stop=True, interval=0)
